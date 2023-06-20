8a74b21a514489997c93befb1f1f3b2178b23180
paul14930 <paul.pelerins@syndicompare.com>
2023-06-20 17:42:26 +0200

    edit destroy ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :voitures
   devise_for :users
   root to: "voitures#index"
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+
+  end
+  get 'current_user_car', to: 'voitures#current_user_car'
+  # get 'voitures/:id/edit', to: 'voitures#edit', as: 'edit_voiture'
+  delete 'voitures/:id', to: 'voitures#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root to: "home#index"
   # Defines the root path route ("/")
