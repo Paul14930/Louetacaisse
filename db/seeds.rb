c697a6bfddb1c513919758ecc079c89bfe1c7d94
paul14930 <paul.pelerins@syndicompare.com>
2023-06-22 14:56:27 +0200

    amelioration seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Booking.destroy_all
 
 puts '------------------------------'
 puts 'starting seed'
-nextcar = ["voit1","voit2","voit3","voit4","voit5","voit6","voit7","voit8"]
+nextcar = ["voit1.jpg","voit2.jpg","voit3.jpg","voit4.jpg","voit5.jpg","voit6.jpg","voit7.jpg","voit8.jpg"]
 user = User.new(
   email: "gregory.bail@cogeli.fr",
   nom: Faker::Name.last_name,
@@ -32,7 +32,8 @@ user = User.new(
       prix: Faker::Commerce.price(range: 35..250)
 
     )
-    path_voit = "app/assets/images/#{nextcar.sample}.jpg"
+    path_voit = "#{asset_path(nexcar.sample)}"
+  #  "app/assets/images/#{nextcar.sample}.jpg"
     voiture.user =  user
     voiture.image.attach(
       io: File.open(path_voit),
@@ -43,40 +44,40 @@ user = User.new(
   end
 
 
-15.times do
-  user = User.new(
-    email: Faker::Internet.email,
-    encrypted_password: Faker::Internet.password,
-    nom: Faker::Name.last_name,
-    prenom: Faker::Name.first_name,
-    adresse: Faker::Address.full_address,
-    ville: Faker::Address.city,
-    code_postal: Faker::Address.zip_code,
-    photo_de_profil: Faker::Avatar.image
+# 15.times do
+#   user = User.new(
+#     email: Faker::Internet.email,
+#     encrypted_password: Faker::Internet.password,
+#     nom: Faker::Name.last_name,
+#     prenom: Faker::Name.first_name,
+#     adresse: Faker::Address.full_address,
+#     ville: Faker::Address.city,
+#     code_postal: Faker::Address.zip_code,
+#     photo_de_profil: Faker::Avatar.image
 
-  )
-  user.password = '123456'
-  user.password_confirmation = '123456'
-  user.save!
+#   )
+#   user.password = '123456'
+#   user.password_confirmation = '123456'
+#   user.save!
 
-  3.times do
-    voiture = Voiture.new(
-      marque: Faker::Vehicle.make,
-      modele: Faker::Vehicle.model,
-      description: Faker::Vehicle.standard_specs,
-      annee: Faker::Vehicle.year,
-      prix: Faker::Commerce.price(range: 35..250)
+#   3.times do
+#     voiture = Voiture.new(
+#       marque: Faker::Vehicle.make,
+#       modele: Faker::Vehicle.model,
+#       description: Faker::Vehicle.standard_specs,
+#       annee: Faker::Vehicle.year,
+#       prix: Faker::Commerce.price(range: 35..250)
 
-    )
-    path_voit = "app/assets/images/#{nextcar.sample}.jpg"
-    voiture.user =  user
-    voiture.image.attach(
-      io: File.open(path_voit),
-      filename: '404_image.png',
-      content_type: 'image/png'
-    )
-    voiture.save!
-  end
-end
+#     )
+#     path_voit = "app/assets/images/#{nextcar.sample}.jpg"
+#     voiture.user =  user
+#     voiture.image.attach(
+#       io: File.open(path_voit),
+#       filename: '404_image.png',
+#       content_type: 'image/png'
+#     )
+#     voiture.save!
+#   end
+# end
 puts 'seed finished....'
 puts 'Success'
