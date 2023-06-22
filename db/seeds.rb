2112533c8748e2f149bf4d0741f795df97deffdb
paul14930 <paul.pelerins@syndicompare.com>
2023-06-22 15:09:26 +0200

    on abandonne le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,41 +7,41 @@ User.destroy_all
 puts 'detroy all bookings'
 Booking.destroy_all
 
-puts '------------------------------'
-puts 'starting seed'
-nextcar = ["voit1.jpg","voit2.jpg","voit3.jpg","voit4.jpg","voit5.jpg","voit6.jpg","voit7.jpg","voit8.jpg"]
-user = User.new(
-  email: "gregory.bail@cogeli.fr",
-  nom: Faker::Name.last_name,
-  prenom: Faker::Name.first_name,
-    adresse: Faker::Address.full_address,
-    ville: Faker::Address.city,
-    code_postal: Faker::Address.zip_code,
-    photo_de_profil: Faker::Avatar.image
+# puts '------------------------------'
+# puts 'starting seed'
+# nextcar = ["voit1.jpg","voit2.jpg","voit3.jpg","voit4.jpg","voit5.jpg","voit6.jpg","voit7.jpg","voit8.jpg"]
+# user = User.new(
+#   email: "gregory.bail@cogeli.fr",
+#   nom: Faker::Name.last_name,
+#   prenom: Faker::Name.first_name,
+#     adresse: Faker::Address.full_address,
+#     ville: Faker::Address.city,
+#     code_postal: Faker::Address.zip_code,
+#     photo_de_profil: Faker::Avatar.image
 
-  )
-  user.password = '123456'
-  user.password_confirmation = '123456'
-  user.save!
-  3.times do
-    voiture = Voiture.new(
-      marque: Faker::Vehicle.make,
-      modele: Faker::Vehicle.model,
-      description: Faker::Vehicle.standard_specs,
-      annee: Faker::Vehicle.year,
-      prix: Faker::Commerce.price(range: 35..250)
+#   )
+#   user.password = '123456'
+#   user.password_confirmation = '123456'
+#   user.save!
+#   3.times do
+#     voiture = Voiture.new(
+#       marque: Faker::Vehicle.make,
+#       modele: Faker::Vehicle.model,
+#       description: Faker::Vehicle.standard_specs,
+#       annee: Faker::Vehicle.year,
+#       prix: Faker::Commerce.price(range: 35..250)
 
-    )
-    path_voit = "#{asset_path(nextcar.sample)}"
-  #  "app/assets/images/#{nextcar.sample}.jpg"
-    voiture.user =  user
-    voiture.image.attach(
-      io: File.open(path_voit),
-      filename: '404_image.png',
-      content_type: 'image/png'
-    )
-    voiture.save!
-  end
+#     )
+#     path_voit = "#{asset_path(nextcar.sample)}"
+#   #  "app/assets/images/#{nextcar.sample}.jpg"
+#     voiture.user =  user
+#     voiture.image.attach(
+#       io: File.open(path_voit),
+#       filename: '404_image.png',
+#       content_type: 'image/png'
+#     )
+#     voiture.save!
+#   end
 
 
 # 15.times do
